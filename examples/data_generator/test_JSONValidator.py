6864dc03055dc14ff3c9c69205e5e291ad8bc03a
Kir <kirillkukarcev@gmail.com>
2019-10-30 00:49:08 +0100

    Comments added, also a new test for empty sample generation

--- examples/data_generator/test_JSONValidator.py
+++ examples/data_generator/test_JSONValidator.py
@@ -5,10 +5,25 @@ from json import load
 
 
 class TestJSONValidator(TestCase):
+
+    """
+        Test if JSONValidator class works correctly in the way:
+
+        - Contains the right schema defined in the project
+        - Can correctly detect datasets with the right schema
+        - Can correctly detect datasets with any wrong schema
+        - Does not allow any objects except for dict and strings, which can be parsed into dict after
+    """
+
     def test_get_valid_schema(self):
 
+        """
+            Test if an instantiated object from JSONValidator contains an valid schema set by the project
+        :return:
+        """
+
         # read existing scheme
-        with open("valid_scheme.json") as file:
+        with open("valid_schema.json") as file:
             scheme = load(file)
 
         validator = JSONValidator()
@@ -17,6 +32,11 @@ class TestJSONValidator(TestCase):
         self.assertTrue(is_identical)
 
     def test_validate_right_dataset(self):
+
+        """
+            Test if JSONValidator validate the correct dataset as a right dataset as well
+        :return: None
+        """
 
         # read a sample data
         with open("correct_data.json") as file:
@@ -29,6 +49,11 @@ class TestJSONValidator(TestCase):
 
     def test_validate_wrong_dataset(self):
 
+        """
+            Test if JSONValidator validate the correct dataset as a wrong dataset as well
+        :return: None
+        """
+
         # read a sample data
         with open("wrong_data.json") as file:
             wrong_samples = load(file)
@@ -37,3 +62,21 @@ class TestJSONValidator(TestCase):
         is_not_valid = all([not validator.validate(sample) for sample in wrong_samples])
 
         self.assertTrue(is_not_valid)
+
+
+    def test_validate_not_correct_data(self):
+
+        """
+            Test if JSONValidator raise ValueError on every object which cannot be transformed into dict -> JSON
+        :return: None
+        """
+
+        # Just take any types
+
+        any_wrong_samples = [ object(), 1, "hello world", 123.32, JSONValidator()]
+
+        validator = JSONValidator()
+
+        for wrong_sample in any_wrong_samples:
+            with self.assertRaises(ValueError):
+                validator.validate(wrong_sample)
