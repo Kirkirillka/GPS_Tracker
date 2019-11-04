9e17a0071f7d3467b0f9d59baabcf711a860a3db
Kir <kirillkukarcev@gmail.com>
2019-11-05 00:50:05 +0100

    New <generic> validators. Also a new validator can be created from these.

--- examples/data_generator/validators.py
+++ examples/data_generator/validators.py
@@ -2,10 +2,11 @@ from abc import ABC, abstractmethod
 from typing import  Mapping
 
 import json
-from json.decoder import JSONDecodeError
 
 from deepdiff import DeepDiff
 from jsonschema import Draft7Validator
+
+from utilities import read_schema_from_file
 
 
 class AbstractValidator(ABC):
@@ -26,7 +27,7 @@ class AbstractValidator(ABC):
         pass
 
     @abstractmethod
-    def get_valid_schema(self):
+    def get_schema(self):
         """
             Returns a class or description of how a valid schema should like
         :return: object
@@ -40,8 +41,7 @@ class JSONValidator(AbstractValidator):
         Validate an provided object to be a valid JSON Schema based object.
 
         Can use any JSON schema provided in file.
-        When instantiating an object, provde a path to JSON Schema file (default is "valid_schema.json",
-        that default for payload empty sample)
+        When instantiating an object, provde a path to JSON Schema file
 
         > # Default behavior
         > validator = JSONValidator()
@@ -51,38 +51,20 @@ class JSONValidator(AbstractValidator):
 
     """
 
-    def __init__(self, schema_filepath="valid_schema.json"):
+    def __init__(self, schema: dict):
         """
 
-        :param schema_filepath: a filepath to a JSON Schema stored in a file
+        :param schema: a filepath to a JSON Schema stored in a file or a dict object containing JSON Schema
         """
-        _schema = self._read_schema_from_file(schema_filepath)
+
+        if isinstance(schema, dict):
+            _schema = schema
+        else:
+            raise TypeError("A provided schema is not a dict object!")
         # Use already existing JSON validator
         self.internal_validator = Draft7Validator(_schema)
 
-    @staticmethod
-    def _read_schema_from_file(schema_filepath):
-
-        """
-            Reads a schema from provided file to JSON schema
-
-        :exception IOError:
-            Raise IOError if a file with scheme is not found
-        :return: dict
-        """
-
-        try:
-            with open(schema_filepath) as file:
-                schema = json.load(file)
-
-        except IOError as e:
-            raise FileNotFoundError("A file with schema is not found!")
-        except JSONDecodeError as e:
-            raise e
-
-        return schema
-
-    def get_valid_schema(self):
+    def get_schema(self):
 
         """
             Returns the scheme provided at initialization
@@ -112,8 +94,6 @@ class JSONValidator(AbstractValidator):
         # check if we supplied with a string
         if isinstance(sample, str):
 
-            normalized_sample = None
-
             try:
                 normalized_sample = json.loads(sample)
             except Exception as e:
@@ -123,7 +103,7 @@ class JSONValidator(AbstractValidator):
 
         # check if we supplied with a dict or a sample was normalized
         if not isinstance(sample, dict):
-            raise ValueError(f"A supplied object {type(sample)} cannot be converted into JSON")
+            raise ValueError(f"A supplied object type '{type(sample)}' cannot be converted into JSON")
 
         # Ask a JSON Schema checker implementation to check if it is valid
         decision = self.internal_validator.is_valid(sample)
@@ -131,5 +111,22 @@ class JSONValidator(AbstractValidator):
         return decision
 
 
+class RawPayloadValidator(JSONValidator):
+
+    SCHEMA_FILENAME = "raw_payload_schema.json"
 
 
+    def __init__(self):
+
+        _schema = read_schema_from_file(self.SCHEMA_FILENAME)
+
+        super(RawPayloadValidator, self).__init__(_schema)
+
+
+class WIFIPayloadValidator(JSONValidator):
+    SCHEMA_FILENAME = "wifi_payload_schema.json"
+
+    def __init__(self):
+        _schema = read_schema_from_file(self.SCHEMA_FILENAME)
+
+        super(WIFIPayloadValidator, self).__init__(_schema)
