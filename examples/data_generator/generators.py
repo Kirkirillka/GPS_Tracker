64fe99727a2337ef36df0fad0d3b407651c526d8
Kir <kirillkukarcev@gmail.com>
2019-11-05 00:47:29 +0100

    New <generic> generators.
    You can create a new one easily by subclassing AbstractGenerator (inherit
    interface) or from JSONGenerator (inherit methods, just change class
    methods to adapt to your tasks)

--- examples/data_generator/generators.py
+++ examples/data_generator/generators.py
@@ -1,12 +1,9 @@
 from abc import ABC, abstractmethod
 
-
-from validators import  JSONValidator
-from field_gens import generate_dict_by_mapping
+from field_gens import generate_dict_by_mapping, RAW_PAYLOAD_FIELDS_TEMPLATE, WIFI_PAYLOAD_FIELDS_TEMPLATE
 
 
 class AbstractGenerator(ABC):
-
     """
     Abstract class to describe methods for a generator.
 
@@ -36,56 +33,28 @@ class AbstractGenerator(ABC):
 
 
 class JSONGenerator(AbstractGenerator):
-
     """
-        JSONGenerator is meant to use in case of testing or absence of real devices(Android, UAV) to generate
-        valid JSON payload to be sent to ClientBroker
+        Class JSONGenerator implements method to generate a JSON file from field mapping.
     """
 
     def __init__(self, field_mapping: dict):
-
         self._map = field_mapping
-        self._validator = JSONValidator()
-        self._num = 0
 
     def get(self):
-
         """
             Return one sample in valid JSON Schema
         :return: dict
         """
 
-        if hasattr(self,"get_with_payload"):
-
-            # Not implemented yet
-            # //TODO add implementation to generate packet with payload
-
-            pass
-
-        else:
-
-            # If not implemented, then return a sample without payload
-            return self._get_empty()
-
-    def _get_empty(self):
-        """
-            Return a sample with in valid JSON schema, but without any payload
-        :return: dict
-        """
-
         # Take a new sample
-        empty_sample = generate_dict_by_mapping(self._map)
+        sample = generate_dict_by_mapping(self._map)
 
-        # Increase counter
-        self._num += 1
-
-        return empty_sample
+        return sample
 
     def __iter__(self):
         return self
 
     def __next__(self):
-
         """
             Interface for iterating
 
@@ -94,9 +63,52 @@ class JSONGenerator(AbstractGenerator):
 
         :return: dict
         """
-
-        # Increase counter
-        self._num += 1
-
         # Return a sample
         return self.get()
+
+
+class RawPayloadGenerator(JSONGenerator):
+    """
+        Class EmptySampleGenerator generates samples with GPS positions, but without any payload.
+    """
+
+    FIELD_MAP = RAW_PAYLOAD_FIELDS_TEMPLATE
+
+    def __init__(self):
+        # Initialize JSON Generator to generate an empty sample
+        self.empty_body_generator = JSONGenerator(RAW_PAYLOAD_FIELDS_TEMPLATE)
+
+    def get(self):
+        # Don't add any other logic, just pass through default JSONGenerator and empty field mapping
+        return self.empty_body_generator.get()
+
+
+class WIFIPayloadGenerator(RawPayloadGenerator):
+    """
+        Class WifiInfoGenerator returns a sample with GPS coordinates and Wifi connection status in this area.
+
+        This works in two steps:
+
+        - First, get an sample without payload from EmptySampleGenerator
+        - Generate a WIFI payload using **WIFI_PAYLOAD_FIELDS_TEMPLATE** filed mapping
+        - Update a "payload" array, insert into empty body message
+    """
+
+    FIELD_MAP = WIFI_PAYLOAD_FIELDS_TEMPLATE
+
+    def __init__(self):
+        # Initialize JSON Generator to generate an empty sample
+        self.empty_body_generator = JSONGenerator(RAW_PAYLOAD_FIELDS_TEMPLATE)
+        self.wifi_payload_generator = JSONGenerator(WIFI_PAYLOAD_FIELDS_TEMPLATE)
+
+    def get(self):
+        # Generate empty body
+        body = self.empty_body_generator.get()
+
+        # Generate WIFI status payload
+        wifi = self.wifi_payload_generator.get()
+
+        # Update payload in main message
+        body['payload'] = wifi
+
+        return body
