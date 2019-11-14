df9d449b3313444065bcb0bfa92e40258fae9d18
Kir <kirillkukarcev@gmail.com>
2019-11-15 00:48:36 +0100

    A class to simulate actions that generates WifiPayload data and publish them on MQTT Message Broker.

---
+++ code/utils/clients.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+from adapters import MQTTBrokerAdapter
+from generators import WIFIPayloadGenerator
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class AbstractClientAppMock(ABC):
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def gen_and_send(self):
+        pass
+
+
+class WIFIClientAppMock(AbstractClientAppMock):
+
+
+    def __init__(self):
+
+        self._generator = WIFIPayloadGenerator()
+        self._mqtt_adapter = MQTTBrokerAdapter()
+
+    def gen_and_send(self) -> bool:
+
+        topic_name = "/messages/"
+
+        logging.debug("Take a message from generator")
+        msg = self._generator.get()
+
+        logging.debug("Sending the message to MQTT Broker")
+        res = self._mqtt_adapter.publish(topic_name, msg)
+
+        return res
+
+
+
+
---
+++ code/utils/tests/test_WIFIClientAppMock.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from utils.clients import WIFIClientAppMock
+
+
+class TestWIFIClientAppMock(TestCase):
+
+    def test_gen_and_send_number(self, number = 1 ):
+
+        client = WIFIClientAppMock()
+
+        for r in range(number):
+            res = client.gen_and_send()
+            self.assertTrue(res)
+
+
+    def test_gen_and_send_30(self):
+
+        self.test_gen_and_send_number(30)
+
+
+    def test_gen_and_send_60(self):
+
+        self.test_gen_and_send_number(60)
+
+
+    def test_gen_and_send_90(self):
+
+        self.test_gen_and_send_number(90)
