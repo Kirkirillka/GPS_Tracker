92cf5b253476016f2cb7f8aa12380d42521a66c5
Kir <kirillkukarcev@gmail.com>
2019-11-16 14:40:48 +0100

    Make DataBroker runner more polite and nicer to Keyboard interrputing with <Ctrl+C>

--- src/runners/databroker_runner.py
+++ src/runners/databroker_runner.py
@@ -1,7 +1,15 @@
+import sys
+
 from databroker.models import DataBroker
 
 if __name__ == '__main__':
 
-    broker = DataBroker()
-    broker.initialize()
-    broker.run_loop()
+    try:
+        broker = DataBroker()
+        broker.initialize()
+        broker.run_loop()
+    except KeyboardInterrupt:
+
+        broker.stop_loop()
+        sys.exit(0)
+
