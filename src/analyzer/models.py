4611e789f2528d041c6298ad544803aa26629486
Kir <kirillkukarcev@gmail.com>
2019-12-06 02:12:10 +0100

    Update .loop implementation for AnalyzerRunner. General updates in AnalyzerRunner.

--- src/analyzer/models.py
+++ src/analyzer/models.py
@@ -3,14 +3,19 @@ from typing import Any
 
 import datetime, time
 
-from placement.solvers.utils import OptimizationScenario
-from placement.solvers.models import UAVPositionSolver
+from analyzer.placement.solvers.utils import OptimizationScenario
+from analyzer.placement.solvers.models import UAVPositionSolver
 
 from storage.models import MongoDBStorageAdapter
 
+# Logging section
+import logging.config
+from utils.logs.tools import read_logging_config
+logging.config.dictConfig(read_logging_config())
+logger = logging.getLogger(__name__)
+
 
 class AbstractAnalyzer(ABC):
-
     """
     Class **AbstractAnalyzer** defines main methods and attributes that will have every Analyzer.
 
@@ -28,12 +33,10 @@ class AbstractAnalyzer(ABC):
 
     @abstractmethod
     def analyze(self, data: Any):
-
         raise NotImplementedError
 
 
 class UAVLocationAnalyzer(AbstractAnalyzer):
-
     """
     Performs analysis on the best placement for UAVs.
     As optimization technique, use the power of UAVPositionSolver's optimization interface.
@@ -46,7 +49,6 @@ class UAVLocationAnalyzer(AbstractAnalyzer):
     MESSAGE_TYPE = "estimation"
 
     def analyze(self, scenario: OptimizationScenario):
-
         # ask solver to estimate the optimum position for UAVs
         solver = UAVPositionSolver(scenario)
         optimized_uavs_pos = solver.solve()
@@ -55,7 +57,6 @@ class UAVLocationAnalyzer(AbstractAnalyzer):
 
 
 class AnalyzerRunner:
-
     """
         Class AnalyzerRunner is responsible for running Analyzers in production.
 
@@ -69,15 +70,17 @@ class AnalyzerRunner:
 
     """
 
-    # How long to wait between each analysis steps
-    BASE_WAIT_TIME = 0.5
+
 
     @property
     def wait_time(self):
         # Analyze each 1 second
-        return self.BASE_WAIT_TIME
+        return self.base_wait_time
 
     def __init__(self, analyzer: UAVLocationAnalyzer):
+
+        # How long to wait between each analysis steps
+        self.base_wait_time = 10
 
         # Init connection to a DB
         self.store = MongoDBStorageAdapter()
@@ -86,37 +89,68 @@ class AnalyzerRunner:
     def loop(self):
 
         # Enter loop
+        logging.info(f"Starting loop in {__class__}")
         while True:
 
             # Phase 1. Fetch and prepare the client's UE last positions
-            records = self.store.get_last_coords()
+            records = self.store.get_aggr_per_client()
+            logging.info(f"Received info for {len(records)} UEs ")
 
-            # Prepare data in format [(x1,y1)]
-            only_positions = [(record['latitude'], record['longitude']) for record in records]
+            # Check if we have data to process
+            if records:
 
-            # Phase 2. Ask solver to optimize
-            optimized_results = self.analyzer.analyze(only_positions)
+                # Prepare data in format [(x1,y1)]
+                data_rows = []
 
-            # Phase 3. Save bulk into Storage
+                for record in records:
 
-            # Anchor to the current time
-            estimation_time = datetime.datetime.now()
+                    last_record = record['data'].pop()
 
-            # Enrich data with clients (device) ID and time for estimation
-            databulk = [{
-                "time": estimation_time,
-                "message_type": self.MESSAGE_TYPE,
-                "payload": {
-                    "method": self.analyzer.scenario.method,
-                    "target": self.analyzer.TARGET,
-                    "latitude": r[0],
-                    "longitude": r[1]
+                    only_last_position = (last_record['latitude'], last_record['longitude'])
+
+                    data_rows.append(only_last_position)
+
+                # Phase 3. Prepare OptimizationScenario
+                logging.info("Starting preparing an optimization scenario")
+                scenario = OptimizationScenario()
+                scenario.random_init(
+                    num_nodes=len(data_rows),
+                    num_uavs=min(len(data_rows),2),
+                    base_num=1)
+
+                scenario.nodes_locations = data_rows
+                logging.debug("The optimization scenario is:")
+                logging.debug(scenario)
+
+                # Phase 2. Ask solver to optimize
+                logging.info("Start analysis of the formed scenario.")
+                optimized_results = self.analyzer.analyze(scenario)
+
+                # Phase 3. Save bulk into Storage
+                logging.info("Analysis is done. Now preparing the result to be stored in the Storage.")
+
+                # Anchor to the current time
+                estimation_time = datetime.datetime.now()
+
+                # Enrich data with clients (device) ID and time for estimation
+                estimation = {
+                    "time": estimation_time,
+                    "message_type": self.analyzer.MESSAGE_TYPE,
+                    "payload": {
+                        "method": scenario.method,
+                        "target": self.analyzer.TARGET,
+                        "suggested": [{
+                            "latitude": x[0],
+                            "longitude": x[1],
+                        } for x in optimized_results]
+                    }
                 }
-            } for r in optimized_results]
 
+                logging.debug("The analysis result is:")
+                logging.debug(estimation)
 
-            for estimation in databulk:
                 self.store.add_estimation(estimation)
 
             # Phase 4. Sleep
-            time.sleep(self.analyzer.wait_time)
+            logging.info(f"The cycle is ended. Sleep for {self.wait_time} seconds.")
+            time.sleep(self.wait_time)
