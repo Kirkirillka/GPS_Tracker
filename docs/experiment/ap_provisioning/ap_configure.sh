54c869adc1ee0376a0d5cdd8096e72dba8f4c43f
Kir <kirillkukarcev@gmail.com>
2020-02-03 00:29:09 +0100

    Required updates

--- docs/experiment/ap_provisioning/ap_configure.sh
+++ docs/experiment/ap_provisioning/ap_configure.sh
@@ -1,6 +1,9 @@
 # Start hostapd
 service hostapd start
 
+## Add local A record for CnC server
+echo "192.168.20.1 default\n" >> /etc/hosts
+
 # Setting up AP interface
 ip addr flush dev wlan3
 ip link set up dev wlan3
@@ -10,4 +13,4 @@ ip addr add 192.168.10.1 dev wlan3
 service dnsmasq start
 
 # Enable masquerade on the output interface
-iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o wlan_conf.j2 -j MASQUERADE
